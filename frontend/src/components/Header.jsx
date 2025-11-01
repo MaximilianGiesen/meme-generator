d3ee3c29e6d9a33ffe59b028d860397e2bf8a3c5
MaximilianGcodes <maximilian.giesen@denkwerk.com>
2025-11-01 14:18:14 +0100

    refactored components and added stylings

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import trollFace from "../assets/troll-face.png"
+
+export default function Header() {
+    return(
+        <header className="header">
+            <img
+                src={trollFace}
+                alt="Trollface"
+            />
+            <h1>Meme Generator</h1>
+        </header>
+    )
+}
